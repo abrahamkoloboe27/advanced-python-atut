6a16763c8ab4948b2ad2d7877418aa6ffb1fd466
Abraham KOLOBOE <abrahamkoloboe2000@gmail.com>
2025-08-15 20:10:18 +0100

    refactor(notebook): update Python syntax examples and data structure explanations
    - Improve string manipulation examples with more comprehensive methods
    - Enhance data structure explanations with detailed usage and operations
    - Add more examples for kwargs demonstration
    - Clean up and comment out unused database creation functions in taxi pipeline

--- scripts/taxi_pipeline.py
+++ scripts/taxi_pipeline.py
@@ -6,8 +6,11 @@ import os
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_root)
 
-from src.taxi_pipeline.get_data import taxi_trips_file, taxi_zones_file
-from src.taxi_pipeline.duckdb_utils import create_taxi_trips_table, create_taxi_zone_table,create_nyc_database
+from src.taxi_pipeline.get_data import (
+                        taxi_trips_file,
+                        taxi_zones_file,
+                    )
+#from src.taxi_pipeline.duckdb_utils import create_taxi_trips_table, create_taxi_zone_table,create_nyc_database
 
 
 # Configure logging
@@ -20,6 +23,6 @@ logger = configure_logging(
 if __name__ == "__main__":
     taxi_trips_file()
     taxi_zones_file()
-    create_nyc_database()
-    create_taxi_trips_table()
-    create_taxi_zone_table()
+    # create_nyc_database()
+    # create_taxi_trips_table()
+    # create_taxi_zone_table()
